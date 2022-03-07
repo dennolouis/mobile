f5dbc706a42df318b6c4756e922594fac6d5c3c2
dennolouis <dennolouis@gmail.com>
2022-03-06 19:48:26 -0500

    pulsing works

---
+++ Block Escape/Assets/Scripts/Pulse.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+
+    private void Start()
+    {
+        StartCoroutine("StartPulsing");
+    }
+    private IEnumerator StartPulsing()
+    {
+        for(float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+                (Mathf.Lerp(transform.localScale.x, transform.localScale.x + 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+                (Mathf.Lerp(transform.localScale.y, transform.localScale.y + 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+                (Mathf.Lerp(transform.localScale.z, transform.localScale.z + 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+                );
+            yield return new WaitForSeconds(0.015f);
+        }
+
+        for (float i = 0f; i <= 1f; i += 0.1f)
+        {
+            transform.localScale = new Vector3(
+                (Mathf.Lerp(transform.localScale.x, transform.localScale.x - 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+                (Mathf.Lerp(transform.localScale.y, transform.localScale.y - 0.025f, Mathf.SmoothStep(0f, 1f, i))),
+                (Mathf.Lerp(transform.localScale.z, transform.localScale.z - 0.025f, Mathf.SmoothStep(0f, 1f, i)))
+                );
+            yield return new WaitForSeconds(0.015f);
+        }
+        StartCoroutine("StartPulsing");
+    }
+}

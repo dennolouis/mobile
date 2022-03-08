becec9b3cfd004cc6c49fd63fd8095197d1e3bf2
dennolouis <dennolouis@gmail.com>
2022-03-07 23:38:39 -0500

    Made scenes fade in and out in transitions

---
+++ Block Escape/Assets/LevelChanger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}

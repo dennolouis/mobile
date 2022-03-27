c6c7600db90cbf00afa5d5878f049e44949805b3
dennolouis <dennolouis@gmail.com>
2022-03-27 17:55:51 -0400

    fixed continue ad issues

--- Block Escape/Assets/Scripts/Effects/ContinueTimer.cs
+++ Block Escape/Assets/Scripts/Effects/ContinueTimer.cs
@@ -2,27 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ContinueTimer : MonoBehaviour
 {
     public TextMeshProUGUI timer;
     int time = 5;
+
+    GameFunctions gameFunctions;
+    bool continued = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameFunctions = FindObjectOfType<GameFunctions>();
         timer.text = time.ToString();
         Invoker.InvokeDelayed(Decrement, 1);
     }
 
     public void Decrement()
     {
-        if(time == 0)
+        if (continued)
         {
-            FindObjectOfType<GameFunctions>().ShowGameOverScreen();
+            return;
+        }
+
+        if(time <= 0)
+        {
+            gameFunctions.ShowGameOverScreen();
             return;
         }
         time--;
         timer.text = time.ToString();
         Invoker.InvokeDelayed(Decrement, 1);
+    }
+
+
+    public void Continue()
+    {
+        continued = true;
     }
 }
